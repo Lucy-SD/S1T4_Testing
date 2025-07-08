35123a8d5a9d306cc84d8d545544ce046019f32b
~ Lucy ~ <castrob.lucia@gmail.com>
2025-07-08 13:34:10 +0200

    Add files via upload

---
+++ S1T5_JavaUtils/src/level1/exercise5/User.java
@@ -0,0 +1,17 @@
+package level1.exercise5;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String toString () {
+        return "Nombre: " + this.name + ". Edad: " + this.age + ".\n";
+    }
+}
